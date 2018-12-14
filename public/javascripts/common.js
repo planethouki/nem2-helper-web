99bd07188860666a61182f9af47332eb77ca6344
planethouki <26595148+planethouki@users.noreply.github.com>
2018-12-14 09:58:26 +0900

    all title write.

---
+++ public/javascripts/common.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {};
+    var instances = M.Sidenav.init(elems, options);
+});
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('account', { title: 'Top' });
+    res.render('account', { title: 'Account' });
 });
 
 router.get('/namespace', function(req, res, next) {
-    res.render('namespace', { title: 'Top' });
+    res.render('namespace', { title: 'Namespace & Mosaic' });
+});
+
+router.get('/transaction', function(req, res, next) {
+    res.render('transaction', { title: 'Transaction' });
+});
+
+router.get('/miscellaneous', function(req, res, next) {
+    res.render('miscellaneous', { title: 'Miscellaneous' });
+});
+
+router.get('/network', function(req, res, next) {
+    res.render('network', { title: 'Network Related' });
 });
 
 module.exports = router;
