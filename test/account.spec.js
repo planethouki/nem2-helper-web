8df1f3a3f893fa54c86a6acf00a3809c863a6a1b
planethouki <26595148+planethouki@users.noreply.github.com>
2018-12-30 16:44:22 +0900

    test full array

--- test/account.spec.js
+++ test/account.spec.js
@@ -6,9 +6,11 @@ const chai = require('chai');
 
 describe('account', function() {
 
+    const signPrivate = JSON.parse(fs.readFileSync("test/test-vectors/2.test-sign-private.json"));
+    const keysPrivate = JSON.parse(fs.readFileSync("test/test-vectors/1.test-keys-private-generated.json"));
+
     it('POST /privkey', function(done) {
-        const tvs = JSON.parse(fs.readFileSync("test/test-vectors/1.test-keys-private-generated.json"));
-        const req = tvs.map(tv => {
+        const req = keysPrivate.map(tv => {
             return request(app)
                 .post('/ajax/privkey')
                 .send({privkey: tv.privateKey})
@@ -21,24 +23,24 @@ describe('account', function() {
         });
     });
 
-    it('POST /privkey/sign', function(done) {
-        const tvs = JSON.parse(fs.readFileSync("test/test-vectors/2.test-sign-private.json"));
-        const req = tvs.slice(0,100).map(tv => {
-            return request(app)
-                .post('/ajax/privkey/sign')
-                .send({privkey: tv.privateKey, data: tv.data})
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect({signature: tv.signature});
+    for (let i = 0; i < signPrivate.length / 100; i++) {
+        it(`POST /privkey/sign part:${i}`, function(done) {
+            const req = signPrivate.slice(i*100,(i+1)*100).map(tv => {
+                return request(app)
+                    .post('/ajax/privkey/sign')
+                    .send({privkey: tv.privateKey, data: tv.data})
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .expect({signature: tv.signature});
+            });
+            Promise.all(req).then(result => {
+                done();
+            });
         });
-        Promise.all(req).then(result => {
-            done();
-        });
-    });
+    }
 
     it('GET /pubkey', function(done) {
-        const tvs = JSON.parse(fs.readFileSync("test/test-vectors/1.test-keys-private-generated.json"));
-        const req = tvs.map(tv => {
+        const req = keysPrivate.map(tv => {
             const addressPlain = tv.address;
             return request(app)
                 .get('/ajax/pubkey')
@@ -54,19 +56,20 @@ describe('account', function() {
         });
     });
 
-    it('GET /pubkey/verify', function(done) {
-        const tvs = JSON.parse(fs.readFileSync("test/test-vectors/2.test-sign-private.json"));
-        const req = tvs.slice(0,100).map(tv => {
-            return request(app)
-                .get('/ajax/pubkey/verify')
-                .query({ pubkey: tv.publicKey, data: tv.data, signature: tv.signature })
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .expect({result: "success"});
+    for (let i = 0; i < signPrivate.length / 100; i++) {
+        it(`GET /pubkey/verify part:${i}`, function (done) {
+            const req = signPrivate.slice(i*100,(i+1)*100).map(tv => {
+                return request(app)
+                    .get('/ajax/pubkey/verify')
+                    .query({pubkey: tv.publicKey, data: tv.data, signature: tv.signature})
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .expect({result: "success"});
+            });
+            Promise.all(req).then(result => {
+                done();
+            });
         });
-        Promise.all(req).then(result => {
-            done();
-        });
-    });
+    }
 
 });
--- test/miscellaneous.spec.js
+++ test/miscellaneous.spec.js
@@ -6,19 +6,22 @@ const chai = require('chai');
 
 describe('miscellaneous', function() {
 
-    it('GET /sha3/256', function(done) {
-        const tvs = JSON.parse(fs.readFileSync("test/test-vectors/0.test-sha3-256.json"));
-        const req = tvs.slice(0,100).map(tv => {
-            return request(app)
-                .get('/ajax/sha3/256')
-                .query({ payload: tv.data })
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .expect({hash: tv.sha3});
+    const sha3_256 = JSON.parse(fs.readFileSync("test/test-vectors/0.test-sha3-256.json"));
+
+    for (let i = 0; i < sha3_256.length / 100; i++) {
+        it(`GET /sha3/256 part:${i}`, function (done) {
+            const req = signPrivate.slice(i * 100, (i + 1) * 100).map(tv => {
+                return request(app)
+                    .get('/ajax/sha3/256')
+                    .query({payload: tv.data})
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .expect({hash: tv.sha3});
+            });
+            Promise.all(req).then(result => {
+                done();
+            });
         });
-        Promise.all(req).then(result => {
-            done();
-        });
-    });
+    }
 
 });
