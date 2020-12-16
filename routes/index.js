cc0a49306c031c02bd50b0eb4cc1d387b53cd45a
planethouki <26595148+planethouki@users.noreply.github.com>
2020-12-16 22:55:48 +0900

    delete unused route

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,4 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/namespace', function(req, res, next) {
-    res.render('namespace', { title: 'Namespace & Mosaic' });
-});
-
-router.get('/transaction', function(req, res, next) {
-    res.render('transaction', { title: 'Transaction' });
-});
-
-router.get('/miscellaneous', function(req, res, next) {
-    res.render('miscellaneous', { title: 'Miscellaneous' });
-});
-
-router.get('/network', function(req, res, next) {
-    res.render('network', { title: 'Network Related' });
-});
-
 module.exports = router;
