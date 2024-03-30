ef6b3a2847e7f6d8246559e9a0fd04c1653c7ade
planethouki <26595148+planethouki@users.noreply.github.com>
2024-03-30 18:43:48 +0900

    navbar

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">nem2-helper</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/account">Account</b-nav-item>
+          <b-nav-item to="/nsmosaic">NS & Mosaic</b-nav-item>
+          <b-nav-item to="/transaction">Transaction</b-nav-item>
+          <b-nav-item to="/others">Others</b-nav-item>
+          <b-nav-item to="/network">Network</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <Nuxt />
+  </div>
+</template>
+<script setup>
+</script>
