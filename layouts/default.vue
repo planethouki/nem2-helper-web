ded5132595fb0dbded7c515975c905c982de11bc
planethouki <26595148+planethouki@users.noreply.github.com>
2024-03-31 21:00:02 +0900

    layout to /

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">nem2-helper</b-navbar-brand>
+      <b-navbar-brand to="/">nem2-helper</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
